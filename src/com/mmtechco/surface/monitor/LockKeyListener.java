6d61df0be831ca9e07260cd97fe10f69fc8918b8
Russell Davies <russell@zeroflux.net>
2012-02-23 17:21:45 +0100

    Bug fix: duplicate lockscreen getting pushed.
    Added a check to make sure that when the volume up is double tapped that
    an additional lockscreen is not pushed when the active screen is the
    lockscreen itself.
    
    To accomplish this, the lockscreen had to be instantiated in the
    LockKeyListener constructor. This makes the app a bit more memory
    intensive and may delay initial startup time. Another solution would be to
    store a flag in the runtime store that updates whenever the screen is
    pushed or popped.

--- src/com/mmtechco/surface/monitor/LockKeyListener.java
+++ src/com/mmtechco/surface/monitor/LockKeyListener.java
@@ -11,6 +11,7 @@ import net.rim.device.api.system.KeyListener;
 import net.rim.device.api.ui.Keypad;
 import net.rim.device.api.ui.Screen;
 import net.rim.device.api.ui.Ui;
+import net.rim.device.api.ui.UiApplication;
 import net.rim.device.api.ui.UiEngine;
 
 public final class LockKeyListener implements KeyListener {
@@ -19,25 +20,30 @@ public final class LockKeyListener implements KeyListener {
 	private static Logger logger = Logger.getInstance();
 	
 	private int lastTime;
-
-	public boolean keyChar(char key, int status, int time) {
-		return false;
+	private Screen lockscreen;
+	
+	public LockKeyListener() {
+		//#ifdef TOUCH
+		lockscreen = new TouchLockScreen();
+		//#else
+		lockscreen = new KeypadLockScreen();
+		//#endif
 	}
-
+	
 	public boolean keyDown(int keycode, int time) {
 		if (Keypad.key(keycode) == Keypad.KEY_VOLUME_UP) {
 			logger.log(TAG, "Volume up caught.");
 			// Continue if second press is within a second
 			if (time - lastTime < 1000) {
-				logger.log(TAG, "Pushing lockscreen");
-				//#ifdef TOUCH
-				Screen lockscreen = new TouchLockScreen();
-				//#else
-				Screen lockscreen = new KeypadLockScreen();
-				//#endif
-				Ui.getUiEngine().pushGlobalScreen(lockscreen,
-						Surface.SCREEN_PRIORITY_LOCKSCREEN,
-						UiEngine.GLOBAL_SHOW_LOWER);
+				logger.log(TAG, "active screen: " + UiApplication.getUiApplication().getActiveScreen());
+				logger.log(TAG, "lock screen:" + lockscreen);
+				// Only push lockscreen if is is not already shown
+				if (!lockscreen.isDisplayed()) {
+					logger.log(TAG, "Pushing lockscreen");
+					Ui.getUiEngine().pushGlobalScreen(lockscreen,
+							Surface.SCREEN_PRIORITY_LOCKSCREEN,
+							UiEngine.GLOBAL_SHOW_LOWER);
+				}
 			}
 			lastTime = time;
 			// Consume event
@@ -56,16 +62,20 @@ public final class LockKeyListener implements KeyListener {
 		}
 		return false;
 	}
+	
+	public boolean keyUp(int keycode, int time) {
+		return false;
+	}
+	
+	public boolean keyChar(char key, int status, int time) {
+		return false;
+	}
 
 	public boolean keyRepeat(int keycode, int time) {
 		return false;
 	}
 
 	public boolean keyStatus(int keycode, int time) {
-		return false;
-	}
-
-	public boolean keyUp(int keycode, int time) {
 		return false;
 	}
 }
